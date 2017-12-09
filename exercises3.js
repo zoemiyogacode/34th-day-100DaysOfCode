6e55481f126414e7486601bf07efaddf228c9dbc
zoemiyogacode <31035249+zoemiyogacode@users.noreply.github.com>
2017-12-09 11:18:43 +0000

    Add files via upload

---
+++ exercises3.js
@@ -0,0 +1,26 @@
+// This took me like 2hours to solve... 
+
+
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ */
+
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code.
+function buildTriangle(num) {
+    var triangle = "";
+    for(var i = 1; i<= num; i++) {
+        triangle += makeLine(i);
+    }
+    return triangle;
+}
+
+console.log(buildTriangle(10));
